5b7c0a337f5206020da5eec9ce17f52810b6ca8d
Andrew Coles <andrew.coles@kibocommerce.com>
2022-08-29 18:41:31 -0500

    fix: update logger to not redact lines with more than 16 total digits

--- __test__/unit/util/logger.spec.ts
+++ __test__/unit/util/logger.spec.ts
@@ -29,6 +29,21 @@ describe('Kibo PaymentGateway Hosting - Logger', () => {
     expect(resultInfo).toEqual(expectMessage)
   })
 
+  it('should not redact a GUID with more than 16 numberic characters', () => {
+    const info = {
+      level: 'info',
+      message: 'Your random guid is 309091E0-231A-43E1-AE32-52347E85C7F4',
+    }
+    const resultInfo = redactPciMessage(info)
+    expect(resultInfo).toEqual(info)
+  })
+
+  it('should not redact a message with 16 digits interspaced with other alphabetic characters', () => {
+    const info = { level: 'info', message: 'Id 4242, User 4242, Code 4242, Country 4242' }
+    const resultInfo = redactPciMessage(info)
+    expect(resultInfo).toEqual(info)
+  })
+
   it('should log json formatted info level message', () => {
     const expected = `{"level":"info","message":"this is a log message"}`
 
